e0f5c38d022b603761c931187c441ff5d8c582b9
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-05-24 10:26:12 -0400

    update range comparator
    Add signed sentinel value
    Change bounds to inclusive healthy range

--- FPGA_Files/CDS-NI_files/Design_Sources/rangeComparator.v
+++ FPGA_Files/CDS-NI_files/Design_Sources/rangeComparator.v
@@ -5,6 +5,6 @@ module rangeComparator(
     input wire signed [15:0] b_max,
     output wire triggered
 );
-    assign triggered = (raw_value == 16'hFFFF) ? 1'b0 : ((raw_value <= b_min) || (raw_value >= b_max));
+    assign triggered = (raw_value == 16'shFFFF) ? 1'b0 : ((raw_value < b_min) || (raw_value > b_max));
 
 endmodule
