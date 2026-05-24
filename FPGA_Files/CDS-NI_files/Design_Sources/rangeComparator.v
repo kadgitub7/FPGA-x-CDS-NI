29f1c9fdef80db2bf9147fd22a923c8e4848645b
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-05-24 10:24:19 -0400

    accommodate negative values and combinational logic
    No need to use always statement with sequential logic when combinational logic is available. Change inputs to signed values so that both are accommodated properly

--- FPGA_Files/CDS-NI_files/Design_Sources/rangeComparator.v
+++ FPGA_Files/CDS-NI_files/Design_Sources/rangeComparator.v
@@ -1,14 +1,10 @@
 `timescale 1ns / 1ps
 module rangeComparator(
-    input wire [15:0] raw_value,
-    input wire [15:0] b_min,
-    input wire [15:0] b_max,
+    input wire signed [15:0] raw_value,
+    input wire signed [15:0] b_min,
+    input wire signed [15:0] b_max,
     output wire triggered
 );
-    always @(*) begin
-        if (raw_value == 16'hFFFF) // This is to check if the raw value is invalid, which is represented by 0xFFFF in our case.
-            triggered = 0; // If the raw value is invalid, we do not want to trigger an alert.
-        else
-            triggered = (raw_value < b_min) || (raw_value > b_max);
-    end
+    assign triggered = (raw_value == 16'hFFFF) ? 1'b0 : ((raw_value <= b_min) || (raw_value >= b_max));
+
 endmodule
