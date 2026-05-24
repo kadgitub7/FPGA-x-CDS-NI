e1ef230a81d2a0a153a9e04d7efb7f0fb5b24629
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-05-24 10:19:36 -0400

    correct rangeComparator module
    make input values 16 bits, fix the 0xFFFF sentinel value to proper verilog syntax, remove assign in always statement for if.

--- FPGA_Files/CDS-NI_files/Design_Sources/rangeComparator.v
+++ FPGA_Files/CDS-NI_files/Design_Sources/rangeComparator.v
@@ -1,13 +1,14 @@
 `timescale 1ns / 1ps
 module rangeComparator(
-    input wire [7:0] raw_value,
-    input wire [7:0] b_min,
-    input wire [7:0] b_max,
+    input wire [15:0] raw_value,
+    input wire [15:0] b_min,
+    input wire [15:0] b_max,
     output wire triggered
 );
-    if (raw_value == 0xFFFF)
-        assign triggered = 0; // If the raw value is invalid, we do not want to trigger an alert.
-    else
-        assign triggered = (raw_value < b_min) || (raw_value > b_max);
-
+    always @(*) begin
+        if (raw_value == 16'hFFFF) // This is to check if the raw value is invalid, which is represented by 0xFFFF in our case.
+            triggered = 0; // If the raw value is invalid, we do not want to trigger an alert.
+        else
+            triggered = (raw_value < b_min) || (raw_value > b_max);
+    end
 endmodule
