a4f7030959d02e7b1d745f0a25f193235ee4251b
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-05-25 09:53:39 -0400

    updated fixed multiply files
    changed combinational logic into clock based execution. This is consistent with the overall plan where multiplication happens in a clock cycle. Also added clk and reset parameters.

--- FPGA_Files/CDS-NI_files/Design_Sources/fixedMultiply.v
+++ FPGA_Files/CDS-NI_files/Design_Sources/fixedMultiply.v
@@ -1,16 +1,28 @@
 `timescale 1ns / 1ps
 
 module fixedMultiply(
+    input clk,
+    input reset,
+
     input wire signed [15:0] a,
     input wire signed [15:0] b,
     input wire valid,
-    output wire signed [31:0] product,
-    output wire result_valid
+
+    output reg signed [31:0] product,
+    output reg result_valid
 );
     //inputs are in the form Q s 1.15
     //output is in the form Q s 2.30
     // * sign takes care of all multiplication as long as signed bits are used and noted
-    assign product = valid ? (a * b) : 32'sh00000000; // If valid is 0 then product is 0
-    assign result_valid = valid; // Output valid is the same as input valid
-
+    always @(posedge clk) begin
+        if (reset) begin
+            product <= 32'sd0;
+            result_valid <= 1'b0;
+        end 
+        else begin
+            product <= valid ? (a * b) : 32'sd0; // If valid is 0 then product is 0
+            result_valid <= valid; // Output valid is the same as input valid
+        end
+    end
+    
 endmodule
