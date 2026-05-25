d1cbd58c96550286657103d9bf2bbbb1df0b325b
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-05-25 10:23:57 -0400

    fixed division module
    made new variable to hold the full length of the multiplication. Previous value is truncated and we would have lost information before truncating. Implemented a two stage pipeline for this where result only appears after second clock run due to this truncation problem

--- FPGA_Files/CDS-NI_files/Design_Sources/fixedDivide.v
+++ FPGA_Files/CDS-NI_files/Design_Sources/fixedDivide.v
@@ -5,24 +5,34 @@ module fixedDivide(
     input reset,
 
     input wire signed [31:0] numerator,
-    input wire signed [15:0] reciprocal_denominator, // This is the precomputed reciprocal of the denominator in Q1.15 format
+    input wire signed [15:0] reciprocal_denominator, // This is the precomputed reciprocal of the denominator in Q2.14 format
     input wire valid,
 
     output reg signed [31:0] quotient,
     output reg result_valid
 );
+    reg signed [47:0] full_product; // Intermediate product with enough bits to hold the result of multiplication before shifting
+    reg valid_stage1; // Register to hold the valid signal for the next stage
+
     // the division performed by this module utilizes a precomputed reciprocal to do fixed point multiplication
     // numerator is in Q2.30 format, reciprocal_denominator is in Q2.14 format, so the product is in Q4.44 format
     // we will then use a right shift to bring it back to Q2.30 format by 14 bits (since we have 14 fractional bits in the reciprocal)
 
     always @(posedge clk) begin
         if (reset) begin
+            full_product <= 48'sd0;
             quotient <= 32'sd0;
             result_valid <= 1'b0;
+            valid_stage1 <= 1'b0;
         end 
         else begin
-            quotient <= valid ? ((numerator * reciprocal_denominator) >>> 14) : 32'sd0; // If valid is 0 then quotient is 0
-            result_valid <= valid; // Output valid is the same as input valid
+            // stage 1, capture multiplication result
+            full_product <= (numerator * reciprocal_denominator); // Multiply numerator by reciprocal of denominator without worrying about losing information
+            valid_stage1 <= valid; // Register the valid signal for the next stage
+
+            // stage 2, use previous cycle's multiplication result
+            quotient <= valid_stage1 ? (full_product >>> 14) : 32'sd0; // If valid is 0 then quotient is 0
+            result_valid <= valid_stage1; // Output valid is the same as input valid
         end
     end
 
